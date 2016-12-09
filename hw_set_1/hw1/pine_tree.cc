0218fcb56c0e1c884eea26a2be205519c19993aa
Alex Carlin <carlin@ucdavis.edu>
2016-12-08 18:31:11 -0800

    Added homework set 1

---
+++ hw_set_1/hw1/pine_tree.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Enter the pine tree's height as an integer: ";
+  int height;
+  std::cin >> height;
+
+  // Assignment:
+  // Write code here to draw a pine tree.
+  // If the height given is "3" then the pine tree should look like this:
+  //   *
+  //  ***
+  // *****
+  //   |
+  //
+  // If the height given is "5" then the pine tree should look like this:
+  //     *
+  //    ***
+  //   *****
+  //  *******
+  // *********
+  //     |
+  //
+  // where the height represents the number of rows of stars.  The trunk should
+  // always be there.  Don't worry about writing code to verify that the input
+  // is an integer (though, kudos if you did think about that being necessary!)
+
+  // if the pine tree is of height n
+  // there will be n rows of stars 
+  // and one tree trunk 
+
+  // for the kth row of stars, the number of stars 
+  // is 2k + 1 
+
+  int final_number_of_stars = ( 2 * height ) + 1; 
+
+  for ( int i=0; i < height; i++ ) {
+    for ( int j=0; j < i; j++ ) {
+      std::cout << '*'; 
+    }
+    std::cout << '\n'; 
+  }
+
+  std::cout << '|'; 
+
+  return 0;
+}
---
+++ hw_set_1/hw3/squarer.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Your assignment is to write a function that takes a float and returns a float
+// and the float that it returns should be the square of the input float.
+// The name of this function must be "square" so that the code in main() below
+// runs succesfully.
+
+int main() {
+
+  std::cout << "Enter a number: ";
+
+  float number;
+  std::cin >> number;
+  std::cout << "The square of " << number << " is: " << square( number ) << std::endl;
+
+  return 0;
+}
